916c599978665239fd04ad91f31dbccfc04f4fa1
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2019-05-15 17:52:14 +0200

    Add blazor layout

---
+++ Distvisor/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Distvisor.Services
+{
+    public class AuthService
+    {
+        private bool isAuthenticated = false;
+
+        public bool IsAuthenticated()
+        {
+            return isAuthenticated;
+        }
+
+        public void Login(string login, string password)
+        {
+            if (login == "admin" && password == "admin")
+                isAuthenticated = true;
+        }
+    }
+}
