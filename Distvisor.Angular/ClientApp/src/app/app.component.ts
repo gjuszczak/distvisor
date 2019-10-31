c80c12e17ab2a755b4ed57be6a2a473128ed4d73
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2019-10-31 16:56:13 +0100

    Change Event emiter to rx

--- Distvisor.Angular/ClientApp/src/app/app.component.ts
+++ Distvisor.Angular/ClientApp/src/app/app.component.ts
@@ -5,9 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
