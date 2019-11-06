30c9b398dca13be0740f23284209e2f073522b44
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2019-11-06 17:37:05 +0100

    sqllite basic integration

---
+++ Distvisor.Web/Data/DistvisorContext.cs
@@ -0,0 +1,16 @@
+﻿using Distvisor.Web.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Distvisor.Web.Data
+{
+    public class DistvisorContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        { 
+            options.UseSqlite(@"Data Source=Data\dev_distvisor.db");
+        }
+    }
+}
--- Distvisor.Web/Startup.cs
+++ Distvisor.Web/Startup.cs
@@ -1,7 +1,9 @@
+using Distvisor.Web.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,7 @@ namespace Distvisor.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DistvisorContext>();
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -29,8 +32,10 @@ namespace Distvisor.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DistvisorContext context)
         {
+            context.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
