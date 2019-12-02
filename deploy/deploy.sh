8136ff8055e837e812e5b18024a62e61731fccd6
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2019-12-02 16:29:50 +0100

    configure deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,1 +1,6 @@
-echo "deploy.sh executed!"
+echo "Deploy started.."
+mkdir -p ./data
+chmod 755 ./data
+docker-compose pull
+docker-compose -p distvisor up --force-recreate --build -d 
+docker image prune -f
