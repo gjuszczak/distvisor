ac3f8f57723ce8520c9dedfd9075e4e623b20fb8
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2020-01-10 11:44:34 +0100

    Hotfix invoices

--- src/Distvisor.Web/ClientApp/src/invoices/invoices/invoices.component.ts
+++ src/Distvisor.Web/ClientApp/src/invoices/invoices/invoices.component.ts
@@ -1,66 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { SelectItem } from 'primeng/api';
-import { InvoicesService } from '../invoices.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-invoices',
   templateUrl: './invoices.component.html'
 })
-export class InvoicesComponent implements OnInit {
-
-  invoices: Invoice[];
-
-  selectedInvoice: Invoice;
-
-  displayDialog: boolean;
-
-  sortOptions: SelectItem[];
-
-  sortKey: string;
-
-  sortField: string;
-
-  sortOrder: number;
-
-  constructor(private invoiceService: InvoicesService) { }
-
-  ngOnInit() {
-    this.invoiceService.getInvoicesLarge().then(invoices => this.invoices = invoices);
-
-    this.sortOptions = [
-      { label: 'Newest First', value: '!year' },
-      { label: 'Oldest First', value: 'year' },
-      { label: 'Brand', value: 'brand' }
-    ];
-  }
-
-  selectInvoice(event: Event, invoice: Invoice) {
-    this.selectedInvoice = invoice;
-    this.displayDialog = true;
-    event.preventDefault();
-  }
-
-  onSortChange(event) {
-    let value = event.value;
-
-    if (value.indexOf('!') === 0) {
-      this.sortOrder = -1;
-      this.sortField = value.substring(1, value.length);
-    }
-    else {
-      this.sortOrder = 1;
-      this.sortField = value;
-    }
-  }
-
-  onDialogHide() {
-    this.selectedInvoice = null;
-  }
-}
-
-export interface Invoice {
-  vin;
-  year;
-  brand;
-  color;
+export class InvoicesComponent {
 }
