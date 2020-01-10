e96bfadd05145dafa8fa9e605d4a54e9e8081534
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2020-01-10 13:01:39 +0100

    Update deploy - migrate db

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
+source .env
 echo "Deploy started.."
 mkdir -p ./data
 chmod 755 ./data
+
+if [ "$DB_UPDATE_STRATEGY" = "MigrateToLatest" ]; then
+   cp ../$PREVIOUS_VERSION/data/distvisor.db ./data/distvisor.db
+   echo "distvisor.db - MigrateToLatest - from ${PREVIOUS_VERSION}"
+fi
+
 docker-compose pull
 docker-compose -p distvisor up --force-recreate --build -d 
 docker image prune -f
