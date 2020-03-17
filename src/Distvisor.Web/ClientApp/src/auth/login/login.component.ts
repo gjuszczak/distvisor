f50428b154c3d54bf7d2ce9eb4bbeb3fe899d913
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2020-03-17 17:59:39 +0100

    working on home page and styling

--- src/Distvisor.Web/ClientApp/src/auth/login/login.component.ts
+++ src/Distvisor.Web/ClientApp/src/auth/login/login.component.ts
@@ -7,7 +7,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit, OnDestroy {
   private returnUrl: string;
